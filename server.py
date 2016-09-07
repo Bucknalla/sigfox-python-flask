93cf5cab6730315775fddf348ac0594558d8de5d
Alex Bucknall <alex.bucknall@gmail.com>
2016-09-07 17:29:09 +0200

    changed port

--- server.py
+++ server.py
@@ -28,4 +28,5 @@ def show_graph():
     return render_template('index.html', temps=tempStore, dates=dateStore)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host= '0.0.0.0',debug=True)
