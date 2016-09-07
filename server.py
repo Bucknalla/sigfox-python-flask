f67385386e72687398d7ece037b36c84ab13fccd
Alex Bucknall <alex.bucknall@gmail.com>
2016-09-07 17:41:32 +0200

    corrected port mistake

--- server.py
+++ server.py
@@ -30,4 +30,4 @@ def show_graph():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host= '0.0.0.0',debug=True)
+    app.run(host= '0.0.0.0',port=port,debug=True)
