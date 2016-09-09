e11d9d389311c4bbd8ad51be27fd3a745a11a997
Alex Bucknall <alex.bucknall@gmail.com>
2016-09-09 18:22:17 +0200

    changed server uri

--- app/server.py
+++ app/server.py
@@ -12,7 +12,8 @@ app.config['BASIC_AUTH_PASSWORD'] = 'twitterbird2016'
 basic_auth = BasicAuth(app)
 
 
-client = MongoClient(os.environ.get(MONGODB_ADDON_URI))
+# client = MongoClient(os.environ.get(MONGODB_ADDON_URI))
+client = MongoClient("mongodb://ugpkibttfjqbtxy:7Wk6HqD8qyK57cBkvRty@bpf07cftaxmub0s-mongodb.services.clever-cloud.com:27017/bpf07cftaxmub0s")
 db = client.tempDatabase
 print 'Connected to Database...'
 collection = db.tempCollection
